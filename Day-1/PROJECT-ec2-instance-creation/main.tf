c8c301654cfb4c3299786e69aea6e86818a474e3
shreyapatilde <161713604+shreyapatilde@users.noreply.github.com>
2024-07-17 01:53:45 +0200

    Update main.tf
    updated ami

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0e872aee57663ae2d"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
